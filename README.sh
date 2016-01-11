b8615c94e6791966c1f95b7c5a1711c13afd6d11
Paulo Koch <paulo.koch@gmail.com>
2016-01-11 00:55:17 -0800

    A bunch of cleanup

--- README.sh
+++ README.sh
@@ -1,18 +1,20 @@
 DIRNAME=$(python -c "import os,sys; print os.path.realpath(sys.argv[1])" $(dirname $0))
-git submodule update --init --recursive
 
-cp profile $HOME/.profile
-cp ackrc $HOME/.ackrc
-cp gitconfig $HOME/.gitconfig
-cp hgrc $HOME/.hgrc
-
-if ! [[ -e ~/Library/"Application Support"/"Sublime Text 2"/Packages/User ]]; then
-    mkdir -p ~/Library/"Application Support"/"Sublime Text 2"/Packages
-    ln -s $DIRNAME/sublime_text_2 ~/Library/"Application Support"/"Sublime Text 2"/Packages/User
-fi
+for COPIED in \
+    'gitconfig' \
+    'hgrc' \
+; do
+    cp $COPIED $HOME/.$COPIED
+done
 
 cd $HOME
-ln -s $DIRNAME/vim .vim
-ln -s .vim/vimrc .vimrc
+for LINKED in \
+    'vim' \
+    'shell_prompt.sh' \
+    'profile' \
+    'ackrc' \
+; do
+    ln -s $DIRNAME/$LINKED .$LINKED
+done
 
 echo "Don't forget to source ~/.profile"
